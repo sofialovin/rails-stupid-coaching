0f6cc1ed015aacaaa6c9de632e87335ca11b7241
Sofia Lovin-Radocea <sofia.lovin1@gmail.com>
2020-10-18 16:24:12 +0200

    address alerts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,3 @@ class PagesController < ApplicationController
     @question = params[:question]
   end
 end
-# If the message is I am going to work, the coach will answer Great!
-# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
-# Otherwise the coach will answer I don't care, get dressed and go to work!
